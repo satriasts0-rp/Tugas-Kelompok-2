8030b5fd96b3b20e46b2e5484de5521c188ab6f2
satriasts0-rp <satriasts0@gmail.com>
2026-06-01 23:08:33 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+use App\Http\Controllers\LoginController;
 
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
