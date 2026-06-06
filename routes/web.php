821216ab433c64b5669feaf74a3e27865a16a47c
Risalagustian5 <muhammadrisalagustian5@gmail.com>
2026-06-06 10:13:21 +0700

    Final Code

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-// 1. Halaman Login Utama (Akses lewat 127.0.0.1:8000/ atau /login)
+// Menampilkan halaman login (welcome.blade.php) saat pertama dibuka
 Route::get('/', function () {
-    return view('welcome'); // Membuka file welcome.blade.php yang ada di folder views
+    return view('welcome'); 
 });
+
 Route::get('/login', function () {
     return view('welcome');
 });
 
-// 2. Halaman Register (Membuka file register yang baru dibuat di dalam folder auth)
+// Menampilkan halaman register di dalam folder auth
 Route::get('/register', function () {
-    return view('auth.register');
+    return view('auth.register'); 
 });
 
-// 3. Halaman Dashboard (Membuka file dashboard.blade.php yang ada di folder views)
+// Menampilkan halaman dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
